135924550e235fb9e972a647bfd4ac711246e2dc
Mangar <Mangar@RYZEN9>
2025-10-17 23:05:14 +0200

    sprt-manager added, adaption incomplete

---
+++ src/qapla-tester/sprt-manager.cpp
@@ -0,0 +1,357 @@
+/**
+ * @license
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Volker Böhm
+ * @copyright Copyright (c) 2025 Volker Böhm
+ */
+
+#include <sstream>
+#include <iomanip>
+#include <ctime>
+#include "epd-reader.h"
+#include "sprt-manager.h"
+#include "game-manager-pool.h"
+#include "logger.h"
+#include "pgn-io.h"
+#include "engine-config-manager.h"
+
+void SprtManager::createTournament(
+    const EngineConfig& engine0, const EngineConfig& engine1, const SprtConfig& config) {
+
+	config_ = config;
+    PgnIO::tournament().initialize("Sprt");
+
+    if (!startPositions_) {
+        startPositions_ = std::make_shared<StartPositions>();
+    }
+
+    if (config.openings.file.empty()) {
+        Logger::testLogger().log("No openings file provided.", TraceLevel::error);
+        return;
+    }
+
+    if (config.openings.format == "epd" || config.openings.format == "raw") {
+        EpdReader reader(config.openings.file);
+        for (const auto& entry : reader.all()) {
+            if (!entry.fen.empty()) {
+                startPositions_->fens.push_back(entry.fen);
+            }
+        }
+    }
+    else if (config.openings.format == "pgn") {
+        PgnIO pgnReader;
+        startPositions_->games = pgnReader.loadGames(config.openings.file);
+    }
+    else {
+        throw AppError::makeInvalidParameters(
+            "Unsupported openings format: " + config.openings.format);
+    }
+
+    PairTournamentConfig ptc;
+    ptc.games = config.maxGames;
+    ptc.repeat = 2;
+    ptc.round = 0;
+    ptc.swapColors = true;
+    ptc.openings = config.openings;
+
+    tournament_.initialize(engine0, engine1, ptc, startPositions_);
+	tournament_.setVerbose(false);
+}
+
+void SprtManager::schedule(const std::shared_ptr<SprtManager>& self, uint32_t concurrency) {
+    sprtCallback_ = InputHandler::getInstance().registerCommandCallback(
+        InputHandler::ImmediateCommand::Info,
+        [this](InputHandler::ImmediateCommand, const InputHandler::CommandValue&) {
+            auto result = getResult();
+            result.printOutcome(std::cout);
+        });
+	auto duel = tournament_.getResult();
+    std::cout << "sprt engines " << duel.getEngineA() << " vs " << duel.getEngineB()
+        << " elo [" << config_.eloLower << ", " << config_.eloUpper << "]"
+        << " alpha " << config_.alpha << " beta " << config_.beta
+        << " maxgames " << config_.maxGames
+        << " concurrency " << concurrency << "\n" << std::flush;
+
+    GameManagerPool::getInstance().setConcurrency(concurrency, true);
+    GameManagerPool::getInstance().addTaskProvider(self, tournament_.getEngineA(), tournament_.getEngineB());
+    GameManagerPool::getInstance().startManagers();
+}
+
+std::optional<GameTask> SprtManager::nextTask() {
+    auto result = computeSprt().first;
+    rememberStop_ = rememberStop_ || result.has_value();
+    if (rememberStop_) {
+        return std::nullopt;
+    }
+
+    return tournament_.nextTask();
+}
+
+void SprtManager::setGameRecord(const std::string& taskId, const GameRecord& record) {
+	bool engine1IsWhite = tournament_.getEngineA().getName() == record.getWhiteEngineName();
+    tournament_.setGameRecord(taskId, record);
+
+    auto [cause, result] = record.getGameResult();
+    auto duel = tournament_.getResult();
+
+    auto [decision, info] = computeSprt();
+
+    std::ostringstream oss;
+    oss << std::left
+        << "  match game " << std::setw(4) << record.getRound()
+        << " result " << std::setw(7) << to_string(engine1IsWhite ? result : switchGameResult(result))
+        << " cause " << std::setw(21) << to_string(cause)
+        << " sprt " << info
+        << " engines " << duel.toString();
+
+    if (!decision_) {
+        Logger::testLogger().log(oss.str(), TraceLevel::result);
+    }
+    if (decision) {
+		if (!decision_) {
+            GameManagerPool::getInstance().stopAll();
+        }
+        decision_ = decision;
+    }
+}
+
+void SprtManager::save(const std::string& filename) const {
+    std::ofstream out(filename);
+    if (!out) {
+        throw std::runtime_error("Failed to open SPRT result file for saving: " + filename);
+    }
+
+    out << tournament_.getEngineA() << "\n";
+    out << tournament_.getEngineB() << "\n";
+    auto section = tournament_.getSectionIfNotEmpty();
+    if (section) {
+        QaplaHelpers::IniFile::saveSection(out, *section);
+    }   
+}
+
+void SprtManager::load(const QaplaHelpers::IniFile::Section& section) {
+    std::string engineA;
+    std::string engineB;
+    uint32_t round = 0;
+    std::string games;
+    // updateCnt_++;
+    try {
+        for (const auto& [key, value]: section.entries) {
+            if (key == "engineA") {
+                engineA = value;
+            } else if (key == "engineB") {
+                engineB = value;
+            } else if (key == "round") {
+                round = std::stoul(value) - 1;
+            } else if (key == "games") {
+                games = value;
+            }
+        }
+        /*
+        for (const auto& pairing : pairings_) {
+            if (!games.empty() && pairing->matches(round, engineA, engineB)) {
+                pairing->fromSection(section);
+                break;
+            }
+        }
+        */
+    } catch (const std::exception& ex) {
+        // Ignore invalid section
+    }
+}
+
+/**
+ * @brief Computes the decision boundaries for the SPRT test.
+ *
+ * Given type I and II error probabilities, this function calculates the lower and upper
+ * log-likelihood ratio bounds used to decide acceptance of H0 or H1 in a sequential test.
+ *
+ * @param alpha Maximum allowed type I error (false positive rate).
+ * @param beta Maximum allowed type II error (false negative rate).
+ * @return std::tuple<double, double> A pair of log-likelihood thresholds (lowerBound, upperBound).
+ */
+static std::tuple<double, double> sprtBounds(double alpha, double beta) {
+    const double lBound = std::log(beta / (1.0 - alpha));         
+    const double uBound = std::log((1.0 - beta) / alpha);         
+    return { lBound, uBound };
+}
+
+/**
+ * @brief Estimates the drawElo parameter based on regularized outcome counts.
+ *
+ * @param wins number of wins.
+ * @param losses number of losses.
+ * @param draws number of draws.
+ * @return double Estimated drawElo value.
+ */
+static double computeDrawElo(double wins, double draws, double losses) {
+    wins += 0.5;
+    draws += 0.5;
+    losses += 0.5;
+
+    auto count = wins + draws + losses;
+    auto pWin = wins / count;
+    auto pLoss = losses / count;
+	return 200.0 * std::log10((1.0 - pLoss) / pLoss * (1.0 - pWin) / pWin);
+}
+
+/**
+ * @brief Computes outcome probabilities based on BayesElo parameters.
+ *
+ * @param bayesElo Elo difference under the hypothesis.
+ * @param drawElo Estimated drawElo value.
+ * @return std::tuple<double, double, double> Probabilities for win, draw and loss.
+ */
+static std::tuple<double, double, double> bayesEloProbabilities(double bayesElo, double drawElo) {
+    const double pWin = 1.0 / (1.0 + std::pow(10.0, (drawElo - bayesElo) / 400.0));
+    const double pLoss = 1.0 / (1.0 + std::pow(10.0, (drawElo + bayesElo) / 400.0));
+    const double pDraw = 1.0 - pWin - pLoss;
+    return { pWin, pDraw, pLoss };
+}
+
+/**
+ * @brief Computes the Log-Likelihood Ratio (LLR) based on observed and expected probabilities.
+ *
+ * @param wins Regularized number of wins.
+ * @param draws Regularized number of draws.
+ * @param losses Regularized number of losses.
+ * @param p0 Tuple of probabilities under H0 (win, loss, draw).
+ * @param p1 Tuple of probabilities under H1 (win, loss, draw).
+ * @return double Computed LLR value.
+ */
+static double computeLLR(double wins, double draws, double losses,
+    const std::tuple<double, double, double>& p0,
+    const std::tuple<double, double, double>& p1) {
+    wins += 0.5;
+	losses += 0.5;
+	draws += 0.5;
+    const auto& [pWin0, pDraw0, pLoss0] = p0;
+    const auto& [pWin1, pDraw1, pLoss1] = p1;
+
+    return 
+        wins * std::log(pWin1 / pWin0) +
+        losses * std::log(pLoss1 / pLoss0) +
+        draws * std::log(pDraw1 / pDraw0);
+}
+
+
+std::pair<std::optional<bool>, std::string> SprtManager::computeSprt(
+    int winsA, int draws, int winsB, const std::string& engineA, const std::string& engineB) const {
+	const double drawElo = computeDrawElo(winsA, draws, winsB);
+
+    const double x = std::pow(10.0, -drawElo / 400.0); 
+	const double xSquare = (x + 1.0) * (x + 1.0); 
+    const double scale = 4.0 * x / xSquare;
+
+    const auto p0 = bayesEloProbabilities(config_.eloLower / scale, drawElo);
+    const auto p1 = bayesEloProbabilities(config_.eloUpper / scale, drawElo);
+
+    const double llr = computeLLR(winsA, draws, winsB, p0, p1);
+    const auto [lBound, uBound] = sprtBounds(config_.alpha, config_.beta);
+
+    if (llr >= uBound) { 
+        return {
+            true,
+            "H1 accepted, " + engineA + " is at least " + std::to_string(config_.eloLower)
+            + " elo stronger than " + engineB
+        };
+    }
+	if (llr <= lBound) {
+        return {
+            false,
+            "H0 accepted, " + engineA + " is not stronger than " + engineB
+            + " by at least " + std::to_string(config_.eloUpper) + " elo."
+        };
+	}
+    std::ostringstream oss;
+    oss << "[ " << std::fixed << std::setprecision(2) << lBound << " < " 
+        << std::setw(5) << llr << " < " << uBound << " ]";
+    return { 
+        std::nullopt, oss.str()
+    };
+}
+
+void SprtManager::runMonteCarloTest(const SprtConfig& config) {
+	config_ = config;
+    constexpr int simulationsPerElo = 1000;
+    constexpr double drawRate = 0.4;
+    constexpr std::array<int, 11> eloDiffs = { -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25 };
+
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+    std::cout << "Running SPRT Monte carlo simulation: "
+        << " | Elo range: [" << config.eloLower << ", " << config.eloUpper << "]"
+        << " | alpha: " << config.alpha << ", beta: " << config.beta
+        << " | maxGames: " << config.maxGames << "\n" << std::flush;
+
+    for (int elo : eloDiffs) {
+        int64_t numH1 = 0;
+        int64_t numH0 = 0;
+		int64_t noDecisions = 0;
+        int64_t totalGames = 0;
+                
+        for (int sim = 0; sim < simulationsPerElo; ++sim) {
+            // Reset intern
+            int winsP1 = 0;
+            int winsP2 = 0;
+            int draws = 0;
+            /*
+			if (sim % 1000 == 0) {
+                std::cout << "Simulation " << sim << " for Elo " << elo << std::endl;
+                double avgGames = (sim > 0) ? static_cast<double>(totalGames) / sim : 0.0;
+                std::cout << elo << ", " << correctDecisions << ", " << avgGames << "\n";
+			}
+            */
+            const double trueScore = 1.0 / (1.0 + std::pow(10.0, -elo / 400.0));
+            const double winProb = (1.0 - drawRate) * trueScore;
+
+            std::optional<bool> decision;
+            uint64_t g = 0;
+
+            for (; g < config_.maxGames; ++g) {
+                double r = static_cast<double>(rand()) / RAND_MAX;
+                if (r < winProb) {
+                    ++winsP1;
+                } else if (r < winProb + drawRate) {
+                    ++draws;
+                } else {
+                    ++winsP2;
+                }
+				auto [result, info] = computeSprt(winsP1, draws, winsP2, "P1", "P2");
+                if (result.has_value()) {
+                    decision = result;
+                    break;
+                }
+            }
+
+			if (!decision) {
+				++noDecisions;
+			}
+            else {
+				numH0 += *decision ? 0 : 1;
+				numH1 += *decision ? 1 : 0;
+            }
+            totalGames += (g + 1);
+        }
+
+        double avgGames = (simulationsPerElo > 0) ? static_cast<double>(totalGames) / simulationsPerElo : 0.0;
+        std::cout << std::fixed << std::setprecision(1)
+            << "Simulated elo difference: " << std::setw(6) << elo
+            << "  No Decisions: " << std::setw(6) << (static_cast<double>(noDecisions) * 100.0) / simulationsPerElo << "%"
+            << "  H0 Accepted: " << std::setw(6) << (static_cast<double>(numH0) * 100.0) / simulationsPerElo << "%"
+            << "  H1 Accepted: " << std::setw(6) << (static_cast<double>(numH1) * 100.0) / simulationsPerElo << "%"
+            << "  Average Games: " << std::setw(6) << avgGames << "\n";
+    }
+}
+
---
+++ src/qapla-tester/sprt-manager.h
@@ -0,0 +1,141 @@
+/**
+ * @license
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Volker Böhm
+ * @copyright Copyright (c) 2025 Volker Böhm
+ */
+
+#pragma once
+
+#include <tuple>
+#include "engine-config.h"
+#include "game-task.h"
+#include "openings.h"
+#include "pair-tournament.h"
+#include "input-handler.h"
+#include "ini-file.h"
+
+/**
+ * @brief Configuration parameters for a SPRT test run.
+ */
+struct SprtConfig {
+    int eloUpper;
+    int eloLower;
+    double alpha;
+    double beta;
+    uint32_t maxGames;
+    Openings openings;
+};
+
+ 
+/**
+  * Manages the analysis of EPD test sets using multiple chess engines in parallel.
+  * Provides GameTasks for engine workers and collects their results.
+  */
+class SprtManager : public GameTaskProvider {
+public:
+    SprtManager() = default;
+
+    /**
+     * @brief Initializes and starts the SPRT testing procedure between two engines.
+     *
+	 * @param engine0 Configuration for the first engine.
+     * @param engine1 Configuration for the second engine.
+     * @param config All configuration parameters required for the SPRT test.
+     */
+    void createTournament(const EngineConfig& engine0, const EngineConfig& engine1,
+        const SprtConfig& config);
+
+    /**
+     * @brief Schedules the tournament and registers all pairings as task providers.
+     *
+     * @param self Shared pointer to this Tournament instance.
+     * @param concurrency Number of parallel workers to use.
+     */
+    void schedule(const std::shared_ptr<SprtManager>& self, uint32_t concurrency);
+
+    /**
+     * @brief Provides the next available task.
+     *
+     * @return A GameTask with a unique taskId or std::nullopt if no task is available.
+     */
+    std::optional<GameTask> nextTask() override;
+
+    /**
+     * @brief Records the result of a finished game identified by taskId.
+     *
+     * @param taskId Identifier of the task this game result belongs to.
+     * @param record Game outcome and metadata.
+     */
+    void setGameRecord(const std::string& taskId, const GameRecord& record) override;
+
+    void runMonteCarloTest(const SprtConfig& config);
+
+	/**
+	 * @brief Returns the current decision of the SPRT test.
+	 * @return std::optional<bool> containing true if H1 accepted, false if H0 accepted, or std::nullopt if inconclusive.
+	 */
+	std::optional<bool> getDecision() const {
+		return decision_;
+	}
+
+    /**
+	 * @brief Saves the current SPRT test state to a stream.
+	 * @param filename The file to save the state to.
+     */
+    void save(const std::string& filename) const;
+
+    /**
+     * @brief Loads the state from a stream - do nothing, if the file cannot be loaded.
+	 * @param filename The file to load the state from.
+     */
+    void load(const QaplaHelpers::IniFile::Section& section);
+
+    TournamentResult getResult() const {
+        TournamentResult t;
+		t.add(tournament_.getResult());
+        return t;
+    }
+
+private:
+    PairTournament tournament_;
+    std::shared_ptr<StartPositions> startPositions_;
+
+    /**
+     * @brief Evaluates the current SPRT test state and logs result if decision boundary is reached.
+     * @return true if the test should be stopped (H0 or H1 accepted), false otherwise.
+     */
+    
+     /**
+      * @brief Computes the result of the Sequential Probability Ratio Test (SPRT) using BayesElo model.
+      *
+      * Applies Jeffreys' prior, estimates drawElo, and compares likelihoods under H0 and H1.
+      * Returns std::optional<bool>: true if H1 accepted, false if H0 accepted, nullopt if inconclusive.
+      */
+    std::pair<std::optional<bool>, std::string> computeSprt() const {
+		auto duel = tournament_.getResult();
+		return computeSprt(duel.winsEngineA, duel.draws, duel.winsEngineB, duel.getEngineA(), duel.getEngineB());
+    }
+    std::pair<std::optional<bool>, std::string> computeSprt(
+        int winsA, int draws, int winsB, const std::string& engineA, const std::string& engineB) const;
+	bool rememberStop_ = false;
+
+    SprtConfig config_;
+	std::optional<bool> decision_ = std::nullopt;
+
+    // Registration
+    std::unique_ptr<InputHandler::CallbackRegistration> sprtCallback_;
+
+};
