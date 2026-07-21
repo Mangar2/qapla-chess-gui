38804f1ebbbbbf881b0b858f0cb612bfd7f99841
Mangar <Mangar@RYZEN9>
2026-07-21 08:17:42 +0200

    Add CMake install script for diagnostic-engine
    This commit introduces a new CMake install script for the diagnostic-engine project. The script sets the installation prefix, handles installation configurations, and manages the installation of executable files. It also includes checks for cross-compilation and ensures proper permissions for installed files. Additionally, it generates an install manifest to track installed components.

---
+++ diagnostic-engine/build/CMakeFiles/diagnostic-engine.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mangar/dev/qapla-chess-gui/diagnostic-engine/diagnostic-engine.cpp" "CMakeFiles/diagnostic-engine.dir/diagnostic-engine.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/diagnostic-engine.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/base-logger.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/base-logger.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/base-logger.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/game-record.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/game-record.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/game-record.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/game-state.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/game-state.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/game-state.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/logger.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/logger.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/logger.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/move-record.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/move-record.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/move-record.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/bitboardmasks.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/bitboardmasks.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/bitboardmasks.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/board.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/board.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/board.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/hashconstants.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/hashconstants.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/hashconstants.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/magics.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/magics.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/magics.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/movegenerator.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/movegenerator.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/movegenerator.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/piecesignature.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/piecesignature.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/piecesignature.cpp.o.d"
+  "/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/pst.cpp" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/pst.cpp.o" "gcc" "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/pst.cpp.o.d"
+  "" "bin/diagnostic-engine" "gcc" "CMakeFiles/diagnostic-engine.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ diagnostic-engine/build/CMakeFiles/diagnostic-engine.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/diagnostic-engine.dir/link.d"
+  "CMakeFiles/diagnostic-engine.dir/diagnostic-engine.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/diagnostic-engine.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/base-logger.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/base-logger.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/game-record.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/game-record.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/game-state.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/game-state.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/logger.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/logger.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/move-record.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/move-record.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/bitboardmasks.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/bitboardmasks.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/board.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/board.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/hashconstants.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/hashconstants.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/magics.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/magics.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/movegenerator.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/movegenerator.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/piecesignature.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/piecesignature.cpp.o.d"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/pst.cpp.o"
+  "CMakeFiles/diagnostic-engine.dir/home/mangar/dev/qapla-chess-gui/extern/qapla-engine-tester/src/qapla-engine/pst.cpp.o.d"
+  "bin/diagnostic-engine"
+  "bin/diagnostic-engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/diagnostic-engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
